7aedb907bd63eb2cab672b992c092f00ba370b9b
alephao <7674479+alephao@users.noreply.github.com>
2024-09-21 13:09:40 -0300

    feat: add 'preload' to rel attribute

--- Sources/Elementary/HtmlAttributes.swift
+++ Sources/Elementary/HtmlAttributes.swift
@@ -214,6 +214,7 @@ public extension HTMLAttributeValue {
 
         public static var icon: Self { "icon" }
         public static var stylesheet: Self { "stylesheet" }
+        public static var preload: Self { "preload" }
     }
 }
 
