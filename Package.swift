071a515d92a12cb006a6191df6fa4f52cc887397
Simon Leeb <52261246+sliemeobn@users.noreply.github.com>
2024-10-08 13:54:48 +0200

    package file confusion

--- Package.swift
+++ Package.swift
@@ -1,13 +1,22 @@
 // swift-tools-version: 5.10
+import Foundation
 import PackageDescription
 
-let featureFlags: [SwiftSetting] = [
-    .enableExperimentalFeature("StrictConcurrency=complete"),
-    .enableUpcomingFeature("StrictConcurrency=complete"),
-    .enableUpcomingFeature("ExistentialAny"),
-    .enableUpcomingFeature("ConciseMagicFile"),
-    .enableUpcomingFeature("ImplicitOpenExistentials"),
-]
+let shouldBuildForEmbedded = ProcessInfo.processInfo.environment["EXPERIMENTAL_EMBEDDED_WASM"].flatMap(Bool.init) ?? false
+
+let featureFlags: [SwiftSetting] = shouldBuildForEmbedded ?
+    [
+        .unsafeFlags([
+            "-Xfrontend", "-emit-empty-object-file",
+        ]),
+    ] :
+    [
+        .enableExperimentalFeature("StrictConcurrency=complete"),
+        .enableUpcomingFeature("StrictConcurrency=complete"),
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("ConciseMagicFile"),
+        .enableUpcomingFeature("ImplicitOpenExistentials"),
+    ]
 
 let package = Package(
     name: "elementary",
